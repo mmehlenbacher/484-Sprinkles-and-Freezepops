9ae99b55b422a195cab8c221c9099f335a5df8dc
marc mehlenbacher <mmehlenbacher23@gmail.com>
2013-04-24 13:20:22 -0400

    TEST
    t

---
+++ admin.php
@@ -0,0 +1,7 @@
+<?php
+    require_once('utility.php');
+    construct("Admin");
+    echo'<h2>Admin Page</h2>';
+    build_admin();
+    build_footer();
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	require_once('utility.php');
+        $db_host = "localhost";
+        $db_user = "mlm1034";
+        $db_pass = "mysqlpassword";
+        $db_name = "mlm1034";
+        // Instantiate the mySQLi object
+        ###->
+        $mysqli= new mysqli($db_host,$db_user,$db_pass,$db_name);
+        // Check connection
+        if ( $mysqli->connect_error ) {
+                echo "Connection failed: " . $mysqli->connect_error;
+                exit;
+        }
+	if(!empty($_POST['submit'])){
+		if(isset($_POST['empid'])){
+			if($_POST['empid']=='1234567890'){
+				header('Location:admin.php');
+			}else{
+				header('Location:user.php?id='.$_POST['empid']);
+			}
+		}
+	}
+	/*if($stmt = $mysqli->prepare('SELECT isAdmin FROM Stored_employee WHERE EmpID = ?');
+	if(isset($_GET['add'])){
+		header('location:addEquip.php?add='.$_GET['add']);
+	}
+	if(isset($_GET['view'])){
+		header('location:viewEquip.php?view='.$_GET['view']);
+	}*/
+	construct('Home');
+	build_home();
+	build_footer();
+
+?>
