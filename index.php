53bcbead91fad6c5bfcd69b108fdb2fd0f0633e0
Elli <elli.fournier@smartbear.com>
2020-02-21 11:22:38 -0500

    1

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 	require_once('utility.php');
         $db_host = "localhost";
-<<<<<<< HEAD
-        $db_user = "mlm214w1034";
-=======
-        $db_user = "mlmw2221034";
->>>>>>> 5ffa29f68a0b51a148f536e696e5d781f9dd0948
+        $db_user = "mlmw22210111134";
         $db_pass = "mysqlpassword122222rtr230034300033f3pppaaa";
 
         // Instantiate the mySQLi object
