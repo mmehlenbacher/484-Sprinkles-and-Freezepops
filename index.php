531ea634db74e2a9a012591c13416102553829b7
Elli Fournier <ellidesu@gmail.com>
2020-03-13 09:02:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         $mysqli= new mysqli($db_host,$db_user,$db_pass,$db_name);
         // Check connection
         if ( $mysqli->connect_error ) {
-                echo "Connection faaaai123led: " . $mysqli->connect_error;
+                echo "Connection faaaai1eee23led: " . $mysqli->connect_error;
                 exit;
         }
 	if(!empty($_POST['submit'])){
